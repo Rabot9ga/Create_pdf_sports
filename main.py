83f35a01b7f7ec8481ab7fe7c52ef38cfec84fc8
Rabot9ga <freedomboxnikita@inbox.ru>
2024-05-01 21:41:10 +0300

    Created data list, starting pdf style

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from reportlab.platypus.tables import Table, TableStyle, colors
 from reportlab.lib.units import cm
 from reportlab.platypus import SimpleDocTemplate
+from reportlab.lib.pagesizes import B4
+from datetime import datetime
 import pandas as pd
 import os
-
 
 
 def creating_dct_fixtures():
@@ -13,6 +14,7 @@ def creating_dct_fixtures():
             data = pd.read_csv(i, sep=",", header=None, index_col=False)
             dict_of_fixtures[i[0:-4]] = data
     return dict_of_fixtures
+
 
 def creating_data_pdf():
     fixtures_data_dct = creating_dct_fixtures()
@@ -24,15 +26,15 @@ def creating_data_pdf():
             headers.extend([i, " ", " "])
     max_len = 0
     for i in fixtures_data_dct:
-        if len(fixtures_data_dct[i][0])>max_len:
+        if len(fixtures_data_dct[i][0]) > max_len:
             max_len = len(fixtures_data_dct[i][0])
     lst_of_datas = [headers]
     for i in range(max_len):
         lst_of_row = []
         for j in fixtures_data_dct:
-            if i>len(fixtures_data_dct[j])-1 and j == list(fixtures_data_dct.keys())[-1]:
+            if i > len(fixtures_data_dct[j]) - 1 and j == list(fixtures_data_dct.keys())[-1]:
                 lst_of_row.extend([" ", " "])
-            elif i>len(fixtures_data_dct[j])-1:
+            elif i > len(fixtures_data_dct[j]) - 1:
                 lst_of_row.extend([" ", " ", " "])
             elif j == list(fixtures_data_dct.keys())[-1]:
                 lst_of_row.extend([fixtures_data_dct[j][0][i], fixtures_data_dct[j][1][i]])
@@ -43,41 +45,34 @@ def creating_data_pdf():
 
 
 def create_pdf():
-    doc = SimpleDocTemplate("trying.pdf", leftMargin=0, topMargin=0)
+    today = datetime.today().strftime('%d-%m-%Y')
+    doc = SimpleDocTemplate(f"fixture_{today}.pdf", leftMargin=20, topMargin=0, pagesize=B4)
     data = creating_data_pdf()
     row_heights = len(data) * [0.37 * cm]
-    row_heights[0] = 20
-    column_width = len(data[0])*[1.3 * cm]
+    row_heights[0] = 30
+    column_width = len(data[0]) * [1.4 * cm]
     t = Table(data, column_width, row_heights)
     t.setStyle(TableStyle([('INNERGRID', (0, 0), (-1, -1), 0.25, colors.grey),
-    ('BOX', (0, 0), (-1, -1), 0.25, colors.grey), ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-    ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'), ('TOPPADDING', (0, 0), (-1, -1), 1), ('BOTTOMPADDING', (0, 0), (-1, -1), 1),
-    ('LEFTPADDING', (0, 0), (-1, -1), 1), ('RIGHTPADDING', (0, 0), (-1, -1), 1)])) # set all table style
+                           ('BOX', (0, 0), (-1, -1), 0.25, colors.grey), ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                           ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'), ('TOPPADDING', (0, 0), (-1, -1), 1),
+                           ('BOTTOMPADDING', (0, 0), (-1, -1), 1),
+                           ('LEFTPADDING', (0, 0), (-1, -1), 1),
+                           ('RIGHTPADDING', (0, 0), (-1, -1), 1)]))  # set all table style
     fixtures_data_dct = creating_dct_fixtures()
     amount_of_sport = len(list(fixtures_data_dct.keys()))
-    t.setStyle(TableStyle([('FONTSIZE', (0, 0), (amount_of_sport*3-1, 0), 15)])) # set headers style
+    t.setStyle(TableStyle([('FONTSIZE', (0, 0), (amount_of_sport * 3 - 1, 0), 15)]))  # set headers style
     for i in range(amount_of_sport):
-        t.setStyle(TableStyle([("SPAN", (i*3, 0), (i*3+1, 0))])) # merge header cells
+        t.setStyle(TableStyle([("SPAN", (i * 3, 0), (i * 3 + 1, 0))]))  # merge header cells
     start = 1
     for i in fixtures_data_dct:
-        step = 99/max(fixtures_data_dct[i][1])
+        step = 99 / max(fixtures_data_dct[i][1])
         for j in range(len(fixtures_data_dct[i][1])):
-            transparency = round(fixtures_data_dct[i][1][j]*step)
+            transparency = round(fixtures_data_dct[i][1][j] * step)
             green_color_bg = colors.HexColor(f"#34a853{transparency:02}", hasAlpha=True)
-            t.setStyle(TableStyle([("BACKGROUND", (start, j+1), (start, j+1), green_color_bg)]))
+            t.setStyle(TableStyle([("BACKGROUND", (start, j + 1), (start, j + 1), green_color_bg)]))
         start += 3
-
-
-
     doc.build([t])
+
 
 if __name__ == "__main__":
     create_pdf()
-
-# t.setStyle(TableStyle(
-#     [("SPAN", (0, 0), (1, 0)), ("SPAN", (3, 0), (4, 0)), ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
-#      ('BOX', (0, 0), (-1, -1), 0.25, colors.black), ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-#      ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'), ('FONTSIZE', (0, 0), (14, 0), 15)]))
-# t.setStyle(TableStyle([("BACKGROUND", (1, 2), (1, 2), green_color_bg)]))
-
-
